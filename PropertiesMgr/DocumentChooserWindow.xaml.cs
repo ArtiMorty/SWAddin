3279d68cbf96c1d7379671358242ccdb45d40753
ArtiMorty <kh.const@gmail.com>
2014-10-30 14:14:23 +0200

    name section works(exept saving)

---
+++ PropertiesMgr/DocumentChooserWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SolidWorks.Interop.sldworks;
+
+namespace PropertiesMgr
+{
+    /// <summary>
+    /// Interaction logic for DocumentChooserWindow.xaml
+    /// </summary>
+    public partial class DocumentChooserWindow : Window
+    {
+        public DocumentChooserWindow(List<ModelDoc2> aDocsList)
+        {
+            InitializeComponent();
+
+            var names = aDocsList.Select(m => m.GetPathName()).ToList();
+
+            ListBoxDocs.ItemsSource = names;
+        }
+
+        public int Index { get; private set; }
+
+        private void ButtonOk_Click(object sender, RoutedEventArgs e)
+        {
+            Index = ListBoxDocs.SelectedIndex;
+            Close();
+        }
+    }
+}
